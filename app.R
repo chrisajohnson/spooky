2507595f9759d88a03cb49913efb8be78ad4a25c
chrisajohnson <johnsonc517@gmail.com>
2021-04-16 10:32:52 -0700

    Candy table and costume graph

--- app.R
+++ app.R
@@ -17,53 +17,53 @@ ui <- fluidPage(
                  selectInput(inputId = "state_select",
                              label = "Choose a state",
                              choices = unique(spooky$state)
-                             ),
+                 ),
                  # Radio button
                  radioButtons(inputId = "region_select",
                               label = "Choose region:",
                               choices = unique(spooky$region_us_census))
-                 ),
+    ),
     mainPanel("put my outputs here",
-    # Table of state's top candies
-    p("State's top candies:"),
-    tableOutput(outputId = "candy_table"),
-    # Graph of costume preference
-    p("Region's top costumes:"),
-    plotOutput(outputId = "costume_graph")
-  )
+              # Table of state's top candies
+              p("State's top candies:"),
+              tableOutput(outputId = "candy_table"),
+              # Graph of costume preference
+              p("Region's top costumes:"),
+              plotOutput(outputId = "costume_graph")
+    )
   )
 )
 
 # Create the server function:
 server <- function(input, output) {
-
-# Reactive object: getting input from user
-state_candy <- reactive({
-  spooky %>%
-    filter(state == input$state_select) %>% # subsetting data
-    select(candy, pounds_candy_sold)
-})
-
-# Make table
-output$candy_table <- renderTable({
-  state_candy()
-})
-
-# Make graph of costume preference
-region_costume <- reactive({
-  spooky %>%
-    filter(region_us_census == input$region_select) %>%
-    count(costume, rank)
-})
-
-
-output$costume_graph <- renderPlot({
-  ggplot(region_costume(), aes(x = costume, y = n)) +
-    geom_col(aes(fill = rank)) +
-    coord_flip() +
-    scale_fill_manual(values = c("black","purple","orange")) +
-    theme_minimal()
-})
+  
+  # Reactive object: getting input from user
+  state_candy <- reactive({
+    spooky %>%
+      filter(state == input$state_select) %>% # subsetting data
+      select(candy, pounds_candy_sold)
+  })
+  
+  # Make table
+  output$candy_table <- renderTable({
+    state_candy()
+  })
+  
+  # Make graph of costume preference
+  region_costume <- reactive({
+    spooky %>%
+      filter(region_us_census == input$region_select) %>%
+      count(costume, rank)
+  })
+  
+  
+  output$costume_graph <- renderPlot({
+    ggplot(region_costume(), aes(x = costume, y = n)) +
+      geom_col(aes(fill = rank)) +
+      coord_flip() +
+      scale_fill_manual(values = c("black","purple","orange")) +
+      theme_minimal()
+  })
 }
 
 # Combine them into an app:
